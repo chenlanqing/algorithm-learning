39c91ce33d13555a29b1ba58d0229a0f67a9601d
Blue_Fish <743633145@qq.com>
2021-11-14 19:08:18 +0800

    linked list leetcode

---
+++ datastructure/src/main/java/com/algorithm/datastructure/linkedlist/ClearValue.java
@@ -0,0 +1,51 @@
+package com.algorithm.datastructure.linkedlist;
+
+import com.algorithm.common.datastructure.primitive.ListNode;
+
+/**
+ * @author QingFan
+ * @version 1.0.0
+ * @date 2021年11月14日 16:44
+ */
+public class ClearValue {
+
+    public ListNode clear(ListNode head, int val) {
+        if (head == null) {
+            return null;
+        }
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode pre = dummy;
+        ListNode cur = dummy.next;
+        ListNode tmp;
+        while (cur != null) {
+            tmp = cur.next;
+            if (cur.val == val) {
+                pre.next = tmp;
+                cur.next = null;
+                cur = tmp;
+            } else {
+                pre = pre.next;
+                cur = tmp;
+            }
+        }
+        return dummy.next;
+    }
+
+    public ListNode clear_1(ListNode head, int val) {
+        if (head == null) {
+            return null;
+        }
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode pre = dummy.next;
+        while (pre.next != null) {
+            if (pre.next.val == val) {
+                pre.next = pre.next.next;
+            } else {
+                pre = pre.next;
+            }
+        }
+        return dummy.next;
+    }
+}
---
+++ datastructure/src/main/java/com/algorithm/datastructure/linkedlist/ReverseKGroup.java
@@ -0,0 +1,16 @@
+package com.algorithm.datastructure.linkedlist;
+
+import com.algorithm.common.datastructure.primitive.ListNode;
+
+/**
+ * @author QingFan
+ * @version 1.0.0
+ * @date 2021年11月14日 13:02
+ */
+public class ReverseKGroup {
+
+    public ListNode inverse(ListNode head, int k) {
+
+        return null;
+    }
+}
